0da6706a0ff2a4d3b53922b2a580f5ba9b1c79a2
Mark Grechanik <drmark@uic.edu>
2022-05-20 18:10:57 -0500

    scope reasoning exercise

---
+++ src/main/java/MoreScopes.java
@@ -0,0 +1,27 @@
+package com.uic.cs474;/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+public class MoreScopes {
+    static {
+        VALUE = 10;//line 3
+    }
+    public MoreScopes(int x) {
+        MoreScopes.VALUE = x;
+    }
+    static {
+        VALUE = 30;//line 7
+    }
+    public static int VALUE = 15;
+
+    public static void main(String[] args) {
+        new MoreScopes(5); //line 10
+        System.out.println(MoreScopes.VALUE);
+    }
+}
