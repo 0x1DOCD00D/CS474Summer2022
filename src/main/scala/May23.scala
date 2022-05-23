932a4e0e25db785ad2b8c72fe2b5fdc4485bb23a
Mark Grechanik <drmark@uic.edu>
2022-05-23 15:54:31 -0500

    may 23

---
+++ src/main/scala/May23.scala
@@ -0,0 +1,17 @@
+package com.uic.cs474
+
+import scala.io.Source
+
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+object May23:
+ // val v: 3 = Source.stdin.mkString(", ").toInt
+  @main def runMay23 = println(v)
