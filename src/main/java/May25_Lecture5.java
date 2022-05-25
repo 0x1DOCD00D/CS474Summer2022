102d01c00530ce24849a43403f886607686427c5
Mark Grechanik <drmark@uic.edu>
2022-05-25 14:31:05 -0500

    May 25

--- src/main/java/May25_Lecture5.java
+++ src/main/java/May25_Lecture5.java
@@ -16,13 +16,77 @@ public class May25_Lecture5 {
         X createHuman();
     }
 
-    class BringGeorgeIntoBeing implements George{
+    interface EmptyInter{}
+    interface XWithDefault{
+        default void f(){
+            System.out.println("default f deom x");
+        }
+    }
+    interface YWithDefault{
+        default void f(){
+            System.out.println("default f from y");
+        }
+    }
+/*
+
+    interface StaticX {
+        static void g();
+    }
+
+    interface StaticY extends StaticX{
+        static void q();
+    }
+
+    class AllStatic implements StaticY{
+
+    }
+*/
+
+//    new Wiero().f()
+    class Wierdo implements XWithDefault, YWithDefault{
+        @Override
+        public void f() {
+            XWithDefault.super.f();
+        }
+    }
+
+    interface Igor extends George, EmptyInter {
+
+    }
+
+    interface V{
+        void f();
+        void g();
+    }
+    interface W {
+        void f();
+    }
+
+    class VW implements W, V {
+        @Override
+        public void g() {
+            System.out.println("g is called");
+        }
+
+        @Override
+        public void f() {
+            System.out.println("f is called");
+        }
+    }
+    class BringGeorgeIntoBeing implements Igor{
         @Override
         public X createHuman() {
             return new X();
         }
     }
     public static void main(String[] args) {
+        VW vw = new May25_Lecture5(). new VW();
+        V v = vw;
+        W w = vw;
+        System.out.println(v.toString());
+        System.out.println(w.toString());
+        w.f();
+        v.f();
         System.out.println(new May25_Lecture5(). new BringGeorgeIntoBeing().createHuman().toString());
     }
 }
