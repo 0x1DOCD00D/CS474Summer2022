7d00d59cddc4bbc4943622c5ed8bebb6bf692779
Mark Grechanik <drmark@uic.edu>
2022-05-29 16:38:23 -0500

    June 1

---
+++ src/main/java/InterfacesStaticAnonymous.java
@@ -0,0 +1,24 @@
+package com.uic.cs474;/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+interface IWithStaticMethod{
+    static void f(){
+        System.out.println("static f");
+    }
+}
+class ImplementStaticMethodInterface implements IWithStaticMethod{
+
+}
+public class InterfacesStaticAnonymous {
+    public static void main(String[] args) {
+//        System.out.println(new ImplementStaticMethodInterface().f());
+        IWithStaticMethod.f();
+    }
+}
