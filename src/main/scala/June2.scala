78daa60de732ee5d7fea58884cf152d56a5f6943
Mark Grechanik <drmark@uic.edu>
2022-05-30 11:04:31 -0500

    June 2

---
+++ src/main/scala/June2.scala
@@ -0,0 +1,20 @@
+package com.uic.cs474
+
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+object June2:
+  def map[A,B](container: List[A])(f: A => B): List[B] =
+    container match
+      case Nil => Nil
+      case head :: tail => f(head) :: map(tail)(f)
+
+  @main def runJune2 =
+    println(map(List(1,2,3))((elem:Int)=>elem.toString.concat((elem+1).toString)))
